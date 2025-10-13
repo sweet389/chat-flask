cb74d64a90ca12b4bdcab8107f835200d3503e5d
sweet389 <pedroadf2010@gmail.com>
2025-10-13 15:01:08 -0300

    errinho basico

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
-from flask import Flask
+from app import app
 from db import db
 from login import lm
 from dotenv import load_dotenv
 import os
 
 load_dotenv(dotenv_path='config.env')
-
-app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB-URL')
-app.config['SECRET_KEY'] = os.getenv('SECRET-KEY')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db.init_app(app)
+app.secret_key=os.getenv('SECRET-KEY')
 lm.init_app(app)
-lm.login_view = 'login'
+db.init_app(app)
+lm.login_view='login'
 
-from app import routes
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=os.getenv('DEBUG'))
+with app.app_context():
+    db.create_all()
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=os.getenv('DEBUG'))
+    
